d2e8a9b7e7ed78be494d0c54d3b3c150df0d23a5
drzzln <drzzln@protonmail.com>
2026-03-15 21:10:13 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,21 @@
 {
   description = "Akeyless gRPC Rust library for interacting with the Akeyless API";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [
-          cargo
-          rustc
-          rustfmt
-          clippy
-          protobuf
-          pkg-config
-          openssl
-        ];
-      };
-    });
+
+  outputs = inputs:
+    (import "${inputs.substrate}/lib/repo-flake.nix" {
+      inherit (inputs) nixpkgs flake-utils;
+    }) {
+      self = inputs.self;
+      language = "rust";
+      description = "Akeyless gRPC Rust library for interacting with the Akeyless API";
+    };
 }
